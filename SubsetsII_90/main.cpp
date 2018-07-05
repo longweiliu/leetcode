34c4c4df109fa915689371f6706941dba901b266
longweiliu <13205162039@163.com>
2018-07-05 18:01:53 +0800

    Validate Binary Search Tree

--- SubsetsII_90/main.cpp
+++ SubsetsII_90/main.cpp
@@ -22,6 +22,29 @@ vector<vector<int>> subsetsWithDup(vector<int> &nums) {
     return res;
 }
 
+void subset(vector<int> &nums, set <vector<int>> &vec) {
+    for (int i = 0; i < nums.size(); ++i) {
+        vector<int> temp;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (i != j) temp.push_back(nums[j]);
+        }
+        subset(temp, vec);
+        vec.insert(temp);
+    }
+}
+
+vector<vector<int>> subsets(vector<int> &nums) {
+    set <vector<int>> s;
+    vector<vector<int>> v;
+    subset(nums, s);
+    s.insert(nums);
+    for (auto vec : s) {
+        v.push_back(vec);
+    }
+    return v;
+}
+
+
 int main() {
     return 0;
 }
--- TwoSum_1/main.cpp
+++ TwoSum_1/main.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 vector<int> twoSum(vector<int> &nums, int target) {
     unordered_map<int, int> hash;
     vector<int> result;
@@ -18,6 +26,70 @@ vector<int> twoSum(vector<int> &nums, int target) {
         hash[nums[i]] = i;
     }
     return result;
+}
+
+void walk(TreeNode *root, unordered_map<TreeNode *, string> &m, string code) {
+    if (!root)
+        return;
+    m[root] = code;
+    walk(root->left, m, code + '0');
+    walk(root->right, m, code + '1');
+}
+
+vector<int> distanceK(TreeNode *root, TreeNode *target, int K) {
+    unordered_map<TreeNode *, string> m;
+    vector<int> result;
+    walk(root, m, "0");
+    string targetCode = m[target];
+    int lenTarget = targetCode.size();
+    for (auto pair : m) {
+        string code = pair.second;
+        int lenCode = code.size();
+        int count = 0;
+        if (lenCode <= lenTarget) {
+            for (int i = 0; i < lenCode; ++i) {
+                if (code[i] == targetCode[i]) {
+                    count++;
+                } else break;
+            }
+        } else {
+            for (int i = 0; i < lenTarget; ++i) {
+                if (code[i] == targetCode[i]) {
+                    count++;
+                } else break;
+            }
+        }
+        if (lenCode + lenTarget - 2 * count == K) {
+            result.push_back(pair.first->val);
+        }
+    }
+    return result;
+
+}
+
+
+bool lemonadeChange(vector<int> &bills) {
+    unordered_map<int, int> m;
+    for (auto bill : bills) {
+        if (bill == 5) {
+            m[5] += 1;
+        } else if (bill == 10) {
+            if (m[5] == 0) return false;
+            else {
+                m[5] -= 1;
+                m[10] += 1;
+            }
+        } else {
+            if (!(m[5] >= 3 || (m[10] >= 1 && m[5] >= 1))) return false;
+            if (m[10] >= 1) {
+                m[10] -= 1;
+                m[5] -= 1;
+            } else {
+                m[5] -= 3;
+            }
+        }
+    }
+    return true;
 }
 
 int main() {
