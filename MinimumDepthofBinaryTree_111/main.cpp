7987c3436b1ef7f9829a26d2259ca577c7643cea
longweiliu <13205162039@163.com>
2018-07-05 20:17:10 +0800

    111. Minimum Depth of Binary Tree

---
+++ MinimumDepthofBinaryTree_111/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int minDepth(TreeNode *root) {
+    if (!root) return 0;
+    if (!root->left) return 1 + minDepth(root->right);
+    if (!root->right) return 1 + minDepth(root->left);
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
+ 
+int main() {
+    return 0;
+}
+
